dff7219f70d2759b9b43b3823b84aa8d37369d72
Christian Abbate <Christian@myabbate.com>
2024-04-06 12:21:55 -0400

    completed storeValue function and alerts for blank entries

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,15 +1,11 @@
 // get all references to DOM elements
-const usernameInput = document.querySelector('#username');
-const titleInput = document.querySelector('#title');
+let usernameInput = document.querySelector('#username');
+let titleInput = document.querySelector('#title');
+let contentInput = document.querySelector('#content');
 const submitBtn = document.querySelector('#submit');
 
 
-usernameInput.textContent = username
-titleInput.textContent = title
 
-allPosts = [];
-userPost = [];
-//keep track of the current user info in local storage
 
 
 
@@ -21,18 +17,25 @@ userPost = [];
 
 
 // create click listener for submit button to save username, title and content to local storage
-submitBtn.addEventListener('click', function (){
-   if (username != null) {
-        localStorage.setItem('Username', username);
-   } if (title != null ) {
-        localStorage.setItem('Title', title);
-   }
+submitBtn.addEventListener('click', function storeInput() {
+     if (usernameInput.value != '') {
+          localStorage.setItem('Username', usernameInput.value);
+     } else {
+          alert('Please enter your username');
+     } if (titleInput.value != '') {
+          localStorage.setItem('Title', titleInput.value);
+     } else {
+          alert('Please enter a title for your post');
+     } if (contentInput.value != '') {
+          localStorage.setItem('Content', contentInput.value);
+     } else {
+          alert('Content cannot be blank');
+     }
 }); 
 
-
 // form reset on button click
-submitBtn.addEventListener('click', function (){
-    document.querySelector('form').reset()
+submitBtn.addEventListener('click', function () {
+     document.querySelector('form').reset()
 })
 
 

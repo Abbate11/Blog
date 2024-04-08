494cdd0609a9dcdbce0d30d47df31c9bda988515
Christian Abbate <Christian@myabbate.com>
2024-04-08 18:26:34 -0400

    started styling landing page and blog post page

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -5,17 +5,19 @@
 const displayNewPost = function () {
     const stringContent = localStorage.getItem('allPosts');
     const newContent = JSON.parse(stringContent);
+    const spot = document.getElementById('main');
 
     for (i = 0; i < newContent.length; i++) {
         let div = document.createElement('div');
-        let title = document.createElement('h1');
+        let title = document.createElement('h2');
         let content = document.createElement('p');
-        let author = document.createElement('h2');
+        let author = document.createElement('h3');
 
         title.textContent = newContent[i].Title;
         content.textContent = newContent[i].Content
-        author.textContent = newContent[i].Username
+        author.textContent = 'Posted by: ' + newContent[i].Username 
 
+        div.setAttribute('class', 'post');
         title.setAttribute('class', 'title');
         content.setAttribute('class', 'content');
         author.setAttribute('class', 'username');
@@ -24,7 +26,7 @@ const displayNewPost = function () {
         div.append(content);
         div.append(author);
 
-        document.body.append(div);
+        spot.append(div);
     };
 }
 
